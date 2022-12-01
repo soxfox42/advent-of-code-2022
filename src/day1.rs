98c74cb560d75a9dba0706a80d267bcbefc1579d
Finn Coffey <finn.coffey@gmail.com>
2022-12-01 21:39:45 +1100

    refactor for 1b

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,14 @@
-pub fn run_a(input: String) {
-    let groups = input
+fn group_sums(input: String) -> Vec<i32> {
+    return input
         .trim()
         .split("\n\n")
-        .map(|group| group.split("\n").map(|cal| cal.parse::<i32>().unwrap()));
-    let sums = groups.map(|group| group.sum::<i32>());
-    println!("Max: {}", sums.max().unwrap());
+        .map(|group| group.split("\n").map(|cal| cal.parse::<i32>().unwrap()))
+        .map(|group| group.sum::<i32>())
+        .collect();
+}
+
+pub fn run_a(input: String) {
+    println!("Max: {}", group_sums(input).iter().max().unwrap());
 }
 
 pub fn run_b(_input: String) {}
