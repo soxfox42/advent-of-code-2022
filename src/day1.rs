8de9ae2321ebaf632370c5707801a46de11186f6
Finn Coffey <finn.coffey@gmail.com>
2022-12-01 21:38:21 +1100

    solve 1a

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,10 @@
 pub fn run_a(input: String) {
+    let groups = input
+        .trim()
+        .split("\n\n")
+        .map(|group| group.split("\n").map(|cal| cal.parse::<i32>().unwrap()));
+    let sums = groups.map(|group| group.sum::<i32>());
+    println!("Max: {}", sums.max().unwrap());
 }
 
-pub fn run_b(input: String) {
-}
+pub fn run_b(_input: String) {}
