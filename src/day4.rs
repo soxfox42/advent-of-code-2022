cb2ff4c48bf970eb68f854a3dc68ea0928456e53
Finn Coffey <finn.coffey@gmail.com>
2022-12-04 21:04:28 +1100

    Solve 4b

--- src/day4.rs
+++ src/day4.rs
@@ -4,6 +4,10 @@ fn subrange(a: &RangeInclusive<u32>, b: &RangeInclusive<u32>) -> bool {
     (a.start() >= b.start() && a.end() <= b.end()) || (b.start() >= a.start() && b.end() <= a.end())
 }
 
+fn overlapping(a: &RangeInclusive<u32>, b: &RangeInclusive<u32>) -> bool {
+    a.start() <= b.end() && a.end() >= b.start()
+}
+
 fn parse_ranges(line: &str) -> Option<(RangeInclusive<u32>, RangeInclusive<u32>)> {
     let mut parts = line.split(&['-', ',']);
     let a_start = parts.next()?.parse().ok()?;
@@ -24,4 +28,13 @@ pub fn run_a(input: &str) {
     );
 }
 
-pub fn run_b(_input: &str) {}
+pub fn run_b(input: &str) {
+    println!(
+        "Overlapping assignments: {}",
+        input
+            .lines()
+            .filter_map(parse_ranges)
+            .filter(|(range_a, range_b)| overlapping(range_a, range_b))
+            .count()
+    );
+}
